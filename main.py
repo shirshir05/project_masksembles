bff0f439994d92639c5b290724b713a83b3e506c
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2021-07-19 17:55:30 +0300

    Added optimization parameters

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import json
 import os
 from time import time
 import numpy as np
+import tensorflow_model_optimization as tfmot
+from tensorflow_model_optimization.python.core.sparsity.keras.prune import prune_low_magnitude
+
+from my_masksembles import MyMasksembles2D, MyMasksembles1D
 
 # TODO pip install git+http://github.com/nikitadurasov/masksembles
 from masksembles.keras import Masksembles1D, Masksembles2D
@@ -9,7 +13,6 @@ from sklearn.metrics import confusion_matrix, precision_recall_curve
 from tensorflow import keras
 import matplotlib.pyplot as plt
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import layers
 # TODO: pip install scikit-optimize
@@ -27,19 +30,24 @@ from sklearn.model_selection import StratifiedKFold
 # TODO: pip install tensorflow_datasets
 import tensorflow_datasets as tfds
 from keras.utils.np_utils import to_categorical
+import tensorflow as tf
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # TODO: code documentation
+# params
 random_state = 42
 best_accuracy = 0.0
+MAX_SAMPLES_NUM = 320
 
 # region dataset
 # all dataset were taken from: https://www.tensorflow.org/datasets/catalog/overview
-datasets_info = dict(imagewang= [14,669, 20],
+datasets_info = dict(cassava=[9430, 5], mnist=[70000, 10], imagewang=[14, 669, 20],
                      binary_alpha_digits=[1404, 36], cifar10=[60000, 10],
-                     citrus_leaves=[759, 4], cassava=[9430, 5], rock_paper_scissors=[2520, 3],
+                     citrus_leaves=[759, 4], rock_paper_scissors=[2520, 3],
                      horses_or_humans=[1280, 2], dmlab=[65550, 6], food101=[75750, 101], cmaterdb=[5000, 10],
                      stl10=[5000, 10], tf_flowers=[2670, 5], cats_vs_dogs=[23262, 2], uc_merced=[2100, 21],
-                     kmnist=[60000, 10], deep_weeds=[17509, 9], eurosat=[27000, 10], mnist=[70000, 10],
+                     kmnist=[60000, 10], deep_weeds=[17509, 9], eurosat=[27000, 10],
                      beans=[1295, 3], svhn_cropped=[73, 257, 10]
                      )  # "dataset_name": [n_samples, NUM_CLASSES]
 
@@ -49,54 +57,84 @@ datasets_info = dict(imagewang= [14,669, 20],
 #   snli= colorectal_histology,  patch_camelyon,oxford_iiit_pet=
 #  ,fashion_mnist = i_naturalist2017 = quickdraw_bitmap,bigearthnet,malaria TODO: X
 
-MAX_SAMPLES_NUM = 320
+
 # endregion
 
-# region hyper-parameters tuned in this project:
-# 1. Learning rate
-# 2. Number of dense layers
-# 3. Number of nodes for each layers
-# 4. Which activation function: 'relu' or sigmoid
-# TODO: Change Hyper-Parameters Tuning values
-dim_learning_rate = Real(low=1e-6, high=1e-1, prior='log-uniform', name='learning_rate')
-dim_num_dense_layers = Integer(low=1, high=10, name='num_dense_layers')
-dim_num_dense_nodes = Integer(low=5, high=512, name='num_dense_nodes')
-dim_activation = Categorical(categories=['relu', 'sigmoid'], name='activation')
+# region hyper-parameters to tune
+learning_rate = Real(low=1e-6, high=1e-1, prior='log-uniform', name='learning_rate')
+n_convolutions = Integer(low=1, high=5, name='n_convolutions')
+n = Integer(low=4, high=8, name='n')  # TODO: Change and Check Tuning values
 
 # hold all examnined hyper-parameters dimention i a list
-dimensions = [dim_learning_rate, dim_num_dense_layers, dim_num_dense_nodes, dim_activation]
-default_parameters = [1e-5, 1, 16, 'relu']
+dimensions = [learning_rate, n_convolutions, n]
+default_parameters = [1e-5, 1, 4]
 
 
 # endregion
 
 # region Config model
-def create_model(learning_rate, num_dense_layers, num_dense_nodes, activation):
-    '''
+def add_dropout(model, model_to_run, dim, n):
+    """
+    the method adds a dropout layer to the input model, according to the wanted model.
+    @param model: model to add dropout layer to
+    @param model_to_run:  string: "basic", "masksembles" or "pruned_masksembles"
+    @param dim: integer, 1 for 1D (after flattening the network) or 2 for 2D.
+    @param n: number of masks, parameter of masksembles
+    """
+    if model_to_run == "basic":
+        model.add(layers.Dropout(0.2))
+    elif model_to_run == "masksembles":
+        if dim == 1:
+            model.add(Masksembles1D(n, 2.0))  # 4
+        else:  # dim==2
+            model.add(Masksembles2D(n, 2.0))
+    else:  # model_to_run is "pruned_masksembles"
+        if dim == 1:
+            model.add(prune_low_magnitude(MyMasksembles1D(n, 2.0)))
+        else:  # dim==2
+            model.add(prune_low_magnitude(MyMasksembles2D(n, 2.0)))
+
+
+def add_convolutions(model, n_convolutions, n_filters):
+    """
+    the method adds convolution layers to the input model
+    @param model: model to add dropout layer to
+    @param n_convolutions: number of convolution layers to add
+    @param n_filters: number of filters of the layers
+    """
+    # Add n_convolutions convolution layers
+    for i in range(n_convolutions):
+        model.add(layers.Conv2D(filters=n_filters, kernel_size=(1, 1), activation="elu"))
+    pass
+
+
+def create_model(learning_rate, n_convolutions, n, model_to_run):
+    """
     The method builds the model according to the examined hyper-parameters.
-    this model was taken from https://github.com/nikitadurasov/masksembles/blob/main/notebooks/MNIST_Masksembles.ipynb
-    '''
+    The basic model was taken from https://github.com/nikitadurasov/masksembles/blob/main/notebooks/MNIST_Masksembles.ipynb
+    @param learning_rate: learning rate to optimize
+    @param n_convolutions: number of convolution layers
+    @param n: number of masks, parameter of masksembles
+    @param model_to_run: string: "basic", "masksembles" or "pruned_masksembles"
+    @return: built model after compilation
+    """
     model = keras.Sequential()
     model.add(keras.Input(shape=input_shape))
 
     model.add(layers.Conv2D(32, kernel_size=(3, 3), activation="elu"))
-    model.add(Masksembles2D(4, 2.0))
+    add_convolutions(model, n_convolutions, 32)
+    add_dropout(model, model_to_run, 2, n)
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
     model.add(layers.Conv2D(64, kernel_size=(3, 3), activation="elu"))
-    model.add(Masksembles2D(4, 2.0))
+    add_convolutions(model, n_convolutions, 64)
+    add_dropout(model, model_to_run, 2, n)
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
     model.add(layers.Flatten())
-    model.add(Masksembles1D(4, 2.))
-    # Add num_dense_layers dense layers
-    for i in range(num_dense_layers):
-        name = 'layer_dense_{0}'.format(i + 1)
-        model.add(Dense(num_dense_nodes,
-                        activation=activation,
-                        name=name))
+    add_dropout(model, model_to_run, 1, n)
     model.add(layers.Dense(n_classes, activation="softmax"))
-    # model.summary()
+    model.summary()
 
     optimizer = Adam(learning_rate=learning_rate)
     # TODO: change metric to maximize
@@ -116,31 +154,24 @@ def divied_4(x_check, y_check):
 
 
 # region fitness - optimization
-'''
-Parameter optimization is based on:
-https://github.com/mardani72/Hyper-Parameter_optimization/blob/master/Hyper_Param_Facies_tf_final.ipynb
-'''
-
-
 @use_named_args(dimensions=dimensions)
-def fitness(learning_rate, num_dense_layers, num_dense_nodes, activation):
+def fitness(learning_rate, n_convolutions, n):
     """
     This function aims to create and train a network with given hyper-parameters
     and then evaluate model performance with the validation dataset.
     It returns fitness value, negative classification accuracy on the dataset.
     It is negative because skpot performs minimization rather than maximization.
+    Parameter optimization is based on:
+    https://github.com/mardani72/Hyper-Parameter_optimization/blob/master/Hyper_Param_Facies_tf_final.ipynb
     Hyper-parameters:
     learning_rate:     Learning-rate for the optimizer.
-    num_dense_layers:  Number of dense layers.
-    num_dense_nodes:   Number of nodes in each dense layer.
-    activation:        Activation function for all layers.
+    n_convolutions:  Number of convolutions layers.
+    n:   n parameter of masksembles.
     """
-
     # Print the hyper-parameters.
     print('learning rate: {0:.1e}'.format(learning_rate))
-    print('num_dense_layers:', num_dense_layers)
-    print('num_dense_nodes:', num_dense_nodes)
-    print('activation:', activation)
+    print('n_convolutions:', n_convolutions)
+    print('n:', n)
     print()
 
     kf = StratifiedKFold(n_splits=3, shuffle=True, random_state=random_state)
@@ -152,9 +183,9 @@ def fitness(learning_rate, num_dense_layers, num_dense_nodes, activation):
 
         # Create the neural network with these hyper-parameters.
         model = create_model(learning_rate=learning_rate,
-                             num_dense_layers=num_dense_layers,
-                             num_dense_nodes=num_dense_nodes,
-                             activation=activation)
+                             n_convolutions=n_convolutions,
+                             n=n,
+                             model_to_run="pruned_masksembles")
         X_train, y_train = divied_4(X_train, y_train)
         X_val, y_val = divied_4(X_val, y_val)
 
@@ -164,7 +195,7 @@ def fitness(learning_rate, num_dense_layers, num_dense_nodes, activation):
                             epochs=100,
                             batch_size=32,
                             validation_data=(X_val, y_val),
-                            # callbacks=[callback_log]
+                            callbacks=[tfmot.sparsity.keras.UpdatePruningStep()]
                             )
 
         # TODO: change metric
@@ -200,12 +231,12 @@ def best_result(search_result, ds_name, index_cv=0):
 
     fig = plot_objective_2D(result=search_result,
                             dimension_identifier1='learning_rate',
-                            dimension_identifier2='num_dense_nodes',
+                            dimension_identifier2='n',
                             levels=50)
     plt.savefig(os.path.join("BayesianOptimization", ds_name, str(index_cv), "Lr_numnods.png"))
     plt.clf()
     # create a list for plotting
-    dim_names = ['learning_rate', 'num_dense_layers', 'num_dense_nodes', 'activation']
+    dim_names = ['learning_rate', 'n_convolutions', 'n']
     plot_objective(result=search_result, dimensions=dim_names)
     plt.savefig(os.path.join("BayesianOptimization", ds_name, str(index_cv), "all_dimen.png"))
     plt.clf()
@@ -252,32 +283,33 @@ def evaluate_on_test(y_true, y_pred, ds_name, index_cv, scores):
     return scores
 
 
+def open_dirs():
+    if not os.path.exists("BayesianOptimization"):
+        os.mkdir("BayesianOptimization")
+    # create in cluster - to prevent error
+    for ds_name in datasets_info:
+        print(f"uploading dataset: {ds_name}")
+        for i in range(10):
+            if not os.path.exists(os.path.join("BayesianOptimization", ds_name)):
+                os.mkdir(os.path.join("BayesianOptimization", ds_name))
+            if not os.path.exists(os.path.join("BayesianOptimization", ds_name, str(i))):
+                os.mkdir(os.path.join("BayesianOptimization", ds_name, str(i)))
+
+
 # ================================== start of the script ==================================
 
-# create BayesianOptimization folder
-if not os.path.exists("BayesianOptimization"):
-    os.mkdir("BayesianOptimization")
+# create BayesianOptimization directories
+# open_dirs()
 
 all_score = {}
-
-# TODO: create in cluster - to prevent error
-# for ds_name in datasets_info:
-#     print(f"uploading dataset: {ds_name}")
-#     for i in range(10):
-#         if not os.path.exists(os.path.join("BayesianOptimization", ds_name)):
-#             os.mkdir(os.path.join("BayesianOptimization", ds_name))
-#         if not os.path.exists(os.path.join("BayesianOptimization", ds_name, str(i))):
-#             os.mkdir(os.path.join("BayesianOptimization", ds_name, str(i)))
-
-
 for ds_name in datasets_info:
     print(f"uploading dataset: {ds_name}")
+
     # constrain the size of train & test sets
     n_samples = MAX_SAMPLES_NUM if datasets_info[ds_name][0] > MAX_SAMPLES_NUM else datasets_info[ds_name][0]
     n_classes = datasets_info[ds_name][1]
 
     # load a sample of the dataset of size n_samples
-
     X, labels = tfds.as_numpy(tfds.load(ds_name,
                                         # split=f'train+test[:{n_samples}]',
                                         split=f'train[:{n_samples}]',
@@ -288,14 +320,13 @@ for ds_name in datasets_info:
     Y = to_categorical(labels, num_classes=n_classes)
     print(f"data shape before: {X.shape}")
 
-    # TODO: preprocess data
+    # preprocess
     X = X / 255
     X = np.resize(X, (X.shape[0], 32, 32, 1))
     print(f"data shape after: {X.shape}")
     input_shape = (X.shape[1], X.shape[2], X.shape[3])
 
     # perform nested cross validation for hyper-parameter optimization and generalization
-    # TODO: change 3 to 10
     kf_external = StratifiedKFold(n_splits=10, shuffle=True, random_state=random_state)
     index_cv = 0
     results = {}
@@ -314,7 +345,6 @@ for ds_name in datasets_info:
             search_result = gp_minimize(func=fitness,
                                         dimensions=dimensions,
                                         acq_func='EI',  # Expected Improvement.
-                                        # TODO change to 50
                                         n_calls=50,
                                         x0=default_parameters)
 
@@ -324,8 +354,7 @@ for ds_name in datasets_info:
             learning_rate = opt_par[0]
             num_layers = opt_par[1]
             num_nodes = opt_par[2]
-            activation = opt_par[3]
-            best_model = create_model(learning_rate, num_layers, num_nodes, activation)
+            best_model = create_model(learning_rate, num_layers, num_nodes)
             X_train_val, Y_train_val = divied_4(X_train_val, Y_train_val)
             start_train = time()
             history = best_model.fit(X_train_val, Y_train_val, epochs=100)
